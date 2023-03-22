6931c4433c0b8a18d44b2b3f1d69044d3bd24a9d
ryangois <ryangoisdev@gmail.com>
2023-03-22 19:33:53 -0300

    import format refactor

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
-import { fetchData, url } from "./api";
-import * as module from "./module";
+import { fetchData, url } from "./api.js";
+import * as module from "./module.js";
 
 
 /**
