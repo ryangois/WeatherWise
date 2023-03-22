34ec9cff49cf4e8392617942680cdce90755acb6
ryangois <ryangoisdev@gmail.com>
2023-03-22 18:14:13 -0300

    module script

--- assets/js/module.js
+++ assets/js/module.js
@@ -81,19 +81,19 @@ export const mps_to_kmh = mps => {
 export const aqiText = {
     1: {
         level: "Good",
-        message: "Air quality is considered satisfactory, and air pollution poses little or no risk"
+        message: "Air quality is considered satisfactory, and air pollution poses little or no risk."
     },
     2: {
         level: "Fair",
-        message: "Air quality is acceptable; However, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution"
+        message: "Air quality is acceptable; However, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution."
     },
     3: {
         level: "Moderate",
-        message: "Members of sensitive groups may experience health effects. The general public is not likely to be affected"
+        message: "Members of sensitive groups may experience health effects. The general public is not likely to be affected."
     },
     4: {
         level: "Poor",
-        message: "Everyone may begin to experience health effects; Members of sensitive groups may experience more serious health effects"
+        message: "Everyone may begin to experience health effects; Members of sensitive groups may experience more serious health effects."
     },
     5: {
         level: "Very Poor",
